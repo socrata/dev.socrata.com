4bead52fdce509859fc5b529a6dee77647067623
peter Moore <peter.moore@socrata.com>
2019-05-23 15:15:37 -0700

    fix NBE tests

--- _tests/test_foundry.rb
+++ _tests/test_foundry.rb
@@ -24,15 +24,15 @@ Capybara.app = eval "Rack::Builder.new {( " + File.read(File.dirname(__FILE__) +
 
 describe '2.1 API, no redirect', :type => :feature, :js => true do
   it "loads and looks as expected" do
-    visit '/foundry/soda.demo.socrata.com/b6kv-3wgw'
+    visit '/foundry/soda.demo.socrata.com/f45n-c7cd'
 
     # Check to make sure we have some elements we expect
     expect(find('#foundry-docs'))
     expect(find('h4.welcome').text).to include('Socrata API, powered by Socrata')
     expect(find('#splash .alert', :visible => false).text).to include('You\'re already using the latest version of this dataset API')
     expect(find('h1#title').text).to include('Old Backend Dataset Name')
-    expect(find('#uid').text).to eq('b6kv-3wgw')
+    expect(find('#uid').text).to eq('f45n-c7cd')
     expect(find('#foundry-docs .description').text).to include('This is the old backend dataset description')
-    expect(find('#foundry-docs .endpoint .target').text).to include('/resource/b6kv-3wgw.json')
+    expect(find('#foundry-docs .endpoint .target').text).to include('/resource/f45n-c7cd.json')
   end
 end
