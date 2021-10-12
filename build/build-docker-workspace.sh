559c59d0e3c862fc61f7b202913593fbb187109c
Peter Squicciarini <stripedpajamas273@gmail.com>
2021-10-12 10:38:29 -0700

    Remove extraneous echos

--- build/build-docker-workspace.sh
+++ build/build-docker-workspace.sh
@@ -5,10 +5,6 @@ BUILD_TAG="${BUILD_TAG:-"$(uuidgen)"}"
 IMAGE_NAME="dev-socrata-build:$(echo "${BUILD_TAG}" | tr ' ' '-')"
 DOCKERFILE="build/Dockerfile"
 
-echo "Building worker image '${IMAGE_NAME}'" >&2
-
-echo "PWD: $(pwd)"
-
 docker build \
   --tag "${IMAGE_NAME}" \
   --file "${DOCKERFILE}" \
